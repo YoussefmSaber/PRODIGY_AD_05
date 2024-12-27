481e9e4ee0a75b25efe69ea93f313cdb76a0bd3f
Youssef Mohammed <youssef.mu.saber@gmail.com>
2024-12-27 09:41:45 +0200

    Initial commit

---
+++ app/src/main/java/com/saber/qrcodescanner/componants/LinkfyText.kt
@@ -0,0 +1,127 @@
+package com.saber.qrcodescanner.componants
+
+import android.util.Log
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.TextUnit
+
+@Composable
+fun LinkifyText(
+    text: String,
+    modifier: Modifier = Modifier,
+    linkColor: Color = Color.Blue,
+    linkEntire: Boolean = false,
+    color: Color = Color.Unspecified,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = null,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    textDecoration: TextDecoration? = null,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    onTextLayout: (TextLayoutResult) -> Unit = {},
+    style: TextStyle = LocalTextStyle.current,
+    clickable: Boolean = true,
+    onClickLink: ((linkText: String) -> Unit)? = null
+) {
+    val uriHandler = LocalUriHandler.current
+    val linkInfos =
+        if (linkEntire) listOf(LinkInfo(text, 0, text.length)) else SpannableStr.getLinkInfos(
+            text
+        )
+    val annotatedString = buildAnnotatedString {
+        append(text)
+        linkInfos.forEach {
+            addStyle(
+                style = SpanStyle(
+                    color = linkColor,
+                    textDecoration = TextDecoration.Underline
+                ),
+                start = it.start,
+                end = it.end
+            )
+            addStringAnnotation(
+                tag = "tag",
+                annotation = it.url,
+                start = it.start,
+                end = it.end
+            )
+        }
+    }
+    if (clickable) {
+        ClickableText(
+            text = annotatedString,
+            modifier = modifier,
+            color = color,
+            fontSize = fontSize,
+            fontStyle = fontStyle,
+            fontWeight = fontWeight,
+            fontFamily = fontFamily,
+            letterSpacing = letterSpacing,
+            textDecoration = textDecoration,
+            textAlign = textAlign,
+            lineHeight = lineHeight,
+            overflow = overflow,
+            softWrap = softWrap,
+            maxLines = maxLines,
+            onTextLayout = onTextLayout,
+            style = style,
+            onClick = { offset ->
+                val item = annotatedString.getStringAnnotations(
+                    start = offset,
+                    end = offset,
+                ).firstOrNull()
+
+                if (linkEntire) {
+                    Log.e("TAG", "LinkifyText: Entire Link")
+                    onClickLink?.invoke("")
+                } else {
+                    if (item != null) {
+                        uriHandler.openUri(item.item)
+                        onClickLink?.invoke(annotatedString.substring(item.start, item.end))
+                    } else {
+                        Log.e("TAG", "LinkifyText: Entire Link")
+                        onClickLink?.invoke("")
+                    }
+                }
+            }
+        )
+    } else {
+        Text(
+            text = annotatedString,
+            modifier = modifier,
+            color = color,
+            fontSize = fontSize,
+            fontStyle = fontStyle,
+            fontWeight = fontWeight,
+            fontFamily = fontFamily,
+            letterSpacing = letterSpacing,
+            textDecoration = textDecoration,
+            textAlign = textAlign,
+            lineHeight = lineHeight,
+            overflow = overflow,
+            softWrap = softWrap,
+            maxLines = maxLines,
+            onTextLayout = onTextLayout,
+            style = style
+        )
+    }
+}
